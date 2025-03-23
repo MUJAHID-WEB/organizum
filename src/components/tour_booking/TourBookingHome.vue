30d40db59f4b08c60f0e42ce66882b44c33b87ca
MUJAHID-WEB <mujahid30390@gmail.com>
2025-03-23 14:47:07 +0600

    about

--- src/components/tour_booking/TourBookingHome.vue
+++ src/components/tour_booking/TourBookingHome.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Header />
     <Hero />
+    <About />
     <HowItWorks />
     <Statistics />
     <Offer />
@@ -13,6 +14,7 @@
 <script>
 import Header from './home/Header.vue';
 import Hero from './home/Hero.vue';
+import About from './home/about.vue';
 import PricingPlans from './home/PricingPlans.vue';
 import HowItWorks from './home/HowItWorks.vue';
 import Statistics from './home/Statistics.vue';
@@ -23,6 +25,7 @@ export default {
   components: {
     Header,
     Hero,
+    About,
     PricingPlans,
     HowItWorks,
     Statistics,
---
+++ src/components/tour_booking/home/about.vue
@@ -0,0 +1,265 @@
+<template>
+  <div class="about_section">
+    <div class="container">
+      <div class="about_wrapper">
+        <div class="about_us">
+          <div class="about_heading">
+            <h4 class="about_subtitle">About</h4>
+            <h2 class="about_title">We Are Bookinpro</h2>
+          </div>
+
+          <div class="about_desc">
+            <div class="desc_img">
+              <img src="/images/tour_booking/about_img.jpg" />
+            </div>
+            <div class="desc_content">
+              <p class="desc">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ncididunt ut labore et olore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ncididunt ut labore et olore magna aliqua.
+              </p>
+              <div class="desc_list">
+                <h3 class="list_title">
+                  we help you planning your journey
+                </h3>
+                <div class="all_list">
+                  <ul class="list_left">
+                    <li>Lorem Ipsum is simply dummy text</li>
+                    <li>Lorem Ipsum is simply dummy text</li>
+                    <li>Lorem Ipsum is simply dummy text</li>
+                  </ul>
+
+                  <ul class="list_right">
+                    <li>Lorem Ipsum is simply dummy text</li>
+                    <li>Lorem Ipsum is simply dummy text</li>
+                    <li>Lorem Ipsum is simply dummy text</li>
+                  </ul>
+
+                </div>
+              </div>
+              <a class="button" href="#modular_pricing">
+                Learn More
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M3.37124 0.500001L12.3712 0.5C12.9235 0.5 13.3712 0.947715 13.3712 1.5L13.3712 10.5C13.3712 11.0523 12.9235 11.5 12.3712 11.5C11.819 11.5 11.3712 11.0523 11.3712 10.5L11.3712 3.91421L2.07835 13.2071C1.68783 13.5976 1.05466 13.5976 0.664138 13.2071C0.273614 12.8166 0.273614 12.1834 0.664138 11.7929L9.95703 2.5L3.37124 2.5C2.81896 2.5 2.37124 2.05228 2.37124 1.5C2.37124 0.947716 2.81896 0.5 3.37124 0.500001Z"
+                    fill="white"></path>
+                </svg>
+              </a>
+            </div>
+
+
+          </div>
+        </div>
+
+        <div class="about_review">
+          <img class="top_qoute" src="/images/tour_booking/h1_quote.png" />
+          <div class="review_track">
+            <div class="review_part">
+              <p class="review">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ncididunt ut labore et olore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ncididunt ut labore et olore magna aliqua.						
+              </p>
+              <div class="star_name">
+                <div class="stars">
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+                </div>
+                <h4 class="reviewer_name">Muntakim</h4>
+                <h5 class="address">Dhaka, Bangladesh</h5>
+
+              </div>
+              <div class="reviewer_img">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"
+                  height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 64 64"
+                  enable-background="new 0 0 64 64" xml:space="preserve">
+                  <g id="Quotemarks-left">
+                    <path
+                      d="M50.6292648,26.225668c-0.1288986-1.3934994-0.0303001-5.1816006,3.5985985-10.4492006   c0.2745018-0.3975,0.2247009-0.9335995-0.1161957-1.2743998c-1.4795036-1.4794998-2.395504-2.4131002-3.0379982-3.0663996   c-0.8448029-0.8614006-1.2305031-1.2539005-1.795002-1.7657003c-0.3769035-0.3388004-0.9472008-0.3446999-1.3281021-0.0125999   c-6.3251991,5.5038996-13.3505974,16.8768997-12.3339958,30.8105011   c0.5956955,8.1815987,6.5634956,14.1200981,14.1894951,14.1200981c7.8260994,0,14.1932983-6.3661995,14.1932983-14.1923981   C63.9993629,32.845768,58.0736694,26.6543674,50.6292648,26.225668z M49.8060646,52.5879669   c-6.5489006,0-11.6767998-5.1581993-12.1953011-12.2645988c0,0,0,0,0-0.0009995   c-1.1435966-15.6709003,8.1718025-25.8496017,10.9863014-28.5449009c0.2743988,0.2705002,0.5878983,0.5887995,1.0498009,1.0594997   c0.5565987,0.5664005,1.3183975,1.3417997,2.4706993,2.4981003c-4.4053001,6.7870998-3.5741997,11.6229992-3.2099991,12.3164005   c0.1728973,0.3290997,0.5273972,0.5508003,0.8984985,0.5508003c6.7236023,0,12.1932983,5.469698,12.1932983,12.1933002   C61.9993629,47.1182671,56.5296669,52.5879669,49.8060646,52.5879669z" />
+                    <path
+                      d="M15.1136675,26.225668c-0.1299-1.3896008-0.0341997-5.1748009,3.5985994-10.4492006   c0.2735004-0.3975,0.2245998-0.9335995-0.1161995-1.2743998c-1.4766006-1.4765997-2.3915997-2.4091997-3.0332003-3.0625   c-0.8476-0.8633003-1.2343998-1.2568998-1.7987995-1.7695999c-0.3769999-0.3388004-0.9473-0.3437004-1.3281002-0.0136003   c-6.3251996,5.5039005-13.3515997,16.875-12.3369999,30.8115005v0.0009995   c0.5977,8.1805992,6.5664001,14.1190987,14.1924,14.1190987c7.8261995,0,14.1934004-6.3661995,14.1934004-14.1923981   C28.4847679,32.8448677,22.5589676,26.6524677,15.1136675,26.225668z M14.2913675,52.5879669   c-6.5478001,0-11.6786995-5.1581993-12.1982002-12.2655983v0.0009995   c-1.1406-15.6748009,8.1747999-25.8516006,10.9892006-28.5459003c0.2754002,0.2705002,0.5899,0.5908003,1.0528002,1.0625   c0.5555992,0.5663996,1.3163996,1.3408003,2.4667988,2.4951c-4.4052992,6.7880993-3.5741997,11.6229992-3.2099991,12.3153992   c0.1729002,0.3291016,0.5283003,0.5518017,0.8993998,0.5518017c6.7237005,0,12.1934004,5.469698,12.1934004,12.1933002   C26.4847679,47.1182671,21.0150681,52.5879669,14.2913675,52.5879669z" />
+                  </g>
+                </svg>
+                <div class="main_img">
+                  <img src="/images/tour_booking/reviewer.jpg" />
+                </div>
+              </div>
+            </div>
+            <div class="review_part">
+              <p class="review">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ncididunt ut labore et olore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ncididunt ut labore et olore magna aliqua.						
+              </p>
+              <div class="star_name">
+                <div class="stars">
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="800px"
+                    width="800px" version="1.1" id="Capa_1" viewBox="0 0 53.867 53.867" xml:space="preserve">
+                    <polygon style="fill:#EFCE4A;"
+                      points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798   10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " />
+                  </svg>
+                </div>
+                <h4 class="reviewer_name">Muntakim</h4>
+                <h5 class="address">Dhaka, Bangladesh</h5>
+
+              </div>
+              <div class="reviewer_img">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"
+                  height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 64 64"
+                  enable-background="new 0 0 64 64" xml:space="preserve">
+                  <g id="Quotemarks-left">
+                    <path
+                      d="M50.6292648,26.225668c-0.1288986-1.3934994-0.0303001-5.1816006,3.5985985-10.4492006   c0.2745018-0.3975,0.2247009-0.9335995-0.1161957-1.2743998c-1.4795036-1.4794998-2.395504-2.4131002-3.0379982-3.0663996   c-0.8448029-0.8614006-1.2305031-1.2539005-1.795002-1.7657003c-0.3769035-0.3388004-0.9472008-0.3446999-1.3281021-0.0125999   c-6.3251991,5.5038996-13.3505974,16.8768997-12.3339958,30.8105011   c0.5956955,8.1815987,6.5634956,14.1200981,14.1894951,14.1200981c7.8260994,0,14.1932983-6.3661995,14.1932983-14.1923981   C63.9993629,32.845768,58.0736694,26.6543674,50.6292648,26.225668z M49.8060646,52.5879669   c-6.5489006,0-11.6767998-5.1581993-12.1953011-12.2645988c0,0,0,0,0-0.0009995   c-1.1435966-15.6709003,8.1718025-25.8496017,10.9863014-28.5449009c0.2743988,0.2705002,0.5878983,0.5887995,1.0498009,1.0594997   c0.5565987,0.5664005,1.3183975,1.3417997,2.4706993,2.4981003c-4.4053001,6.7870998-3.5741997,11.6229992-3.2099991,12.3164005   c0.1728973,0.3290997,0.5273972,0.5508003,0.8984985,0.5508003c6.7236023,0,12.1932983,5.469698,12.1932983,12.1933002   C61.9993629,47.1182671,56.5296669,52.5879669,49.8060646,52.5879669z" />
+                    <path
+                      d="M15.1136675,26.225668c-0.1299-1.3896008-0.0341997-5.1748009,3.5985994-10.4492006   c0.2735004-0.3975,0.2245998-0.9335995-0.1161995-1.2743998c-1.4766006-1.4765997-2.3915997-2.4091997-3.0332003-3.0625   c-0.8476-0.8633003-1.2343998-1.2568998-1.7987995-1.7695999c-0.3769999-0.3388004-0.9473-0.3437004-1.3281002-0.0136003   c-6.3251996,5.5039005-13.3515997,16.875-12.3369999,30.8115005v0.0009995   c0.5977,8.1805992,6.5664001,14.1190987,14.1924,14.1190987c7.8261995,0,14.1934004-6.3661995,14.1934004-14.1923981   C28.4847679,32.8448677,22.5589676,26.6524677,15.1136675,26.225668z M14.2913675,52.5879669   c-6.5478001,0-11.6786995-5.1581993-12.1982002-12.2655983v0.0009995   c-1.1406-15.6748009,8.1747999-25.8516006,10.9892006-28.5459003c0.2754002,0.2705002,0.5899,0.5908003,1.0528002,1.0625   c0.5555992,0.5663996,1.3163996,1.3408003,2.4667988,2.4951c-4.4052992,6.7880993-3.5741997,11.6229992-3.2099991,12.3153992   c0.1729002,0.3291016,0.5283003,0.5518017,0.8993998,0.5518017c6.7237005,0,12.1934004,5.469698,12.1934004,12.1933002   C26.4847679,47.1182671,21.0150681,52.5879669,14.2913675,52.5879669z" />
+                  </g>
+                </svg>
+                <div class="main_img">
+                  <img src="/images/tour_booking/reviewer.jpg" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="track_button">
+            <button class="btn_left">
+
+              <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 14L1 8L7 2" stroke="#000" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round" />
+              </svg>
+
+            </button>
+
+            <button class="btn_right">
+              <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2 14L8 8L2 2" stroke="#000" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round"></path>
+              </svg>
+            </button>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'About',
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const reviewTrack = document.querySelector(".review_track"); // Target the track container
+  const reviews = document.querySelectorAll(".review_part"); // Get all review parts
+  const totalReviews = reviews.length;
+  let currentIndex = 0;
+
+  const btnLeft = document.querySelector(".btn_left");
+  const btnRight = document.querySelector(".btn_right");
+
+  // Disable left button initially
+  btnLeft.disabled = true;
+
+  // Set the gap between reviews
+  const gap = 32; // 32px gap
+
+  function updateSliderPosition() {
+    const reviewWidth = reviews[0].offsetWidth; // Width of one review
+    const offset = (reviewWidth + gap) * currentIndex; // Calculate offset with gap
+    reviewTrack.style.transform = `translateX(-${offset}px)`;
+    updateButtons();
+  }
+
+  function nextSlide() {
+    if (currentIndex < totalReviews - 1) {
+      currentIndex++;
+      updateSliderPosition();
+    }
+  }
+
+  function prevSlide() {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateSliderPosition();
+    }
+  }
+
+  function updateButtons() {
+    // Disable/enable buttons based on currentIndex
+    btnLeft.disabled = currentIndex === 0;
+    btnRight.disabled = currentIndex === totalReviews - 1;
+  }
+
+  // Event listeners for navigation buttons
+  btnLeft.addEventListener("click", function () {
+    prevSlide();
+  });
+
+  btnRight.addEventListener("click", function () {
+    nextSlide();
+  });
+
+  // Update slider position on window resize
+  window.addEventListener("resize", function () {
+    updateSliderPosition();
+  });
+
+  // Initialize
+  updateButtons();
+});
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/tour_booking/_about.scss";
+</style>
