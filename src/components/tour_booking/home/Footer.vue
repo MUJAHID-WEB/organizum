2330719af4ee590d2a961628fc2957bebe75072c
MUJAHID-WEB <mujahid30390@gmail.com>
2025-04-02 11:32:40 +0600

    footer

--- src/components/tour_booking/home/Footer.vue
+++ src/components/tour_booking/home/Footer.vue
@@ -1,7 +1,218 @@
 <template>
   <footer>
-
     <div class="footer-top-main">
+
+      <div class="container">
+        <div class="footer-top-wrapper">
+
+          <div class="footer-top-left">
+            <h2>Get started free schedule</h2>
+            <p>imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer</p>
+          </div>
+
+
+          <div class="footer-top-right">
+
+            <!-- <AppButton text="Try it free" href="#" /> -->
+            <a class="btn" href="#">
+              Try it free
+
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" viewBox="0 0 16 12" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                      d="M9.21967 0.21967C9.51256 -0.0732233 9.98744 -0.0732233 10.2803 0.21967L15.3387 5.278C15.6316 5.5709 15.6316 6.04577 15.3387 6.33866L10.2803 11.397C9.98744 11.6899 9.51256 11.6899 9.21967 11.397C8.92678 11.1041 8.92678 10.6292 9.21967 10.3363L13.7477 5.80833L9.21967 1.28033C8.92678 0.987437 8.92678 0.512563 9.21967 0.21967Z"
+                      fill="white" />
+                    <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd"
+                      d="M0 5.75C0 5.33579 0.335786 5 0.75 5H14.775C15.1892 5 15.525 5.33579 15.525 5.75C15.525 6.16421 15.1892 6.5 14.775 6.5H0.75C0.335786 6.5 0 6.16421 0 5.75Z"
+                      fill="white" />
+                  </svg>
+                </a>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-main">
+
+      <div class="container footer-logo-container">
+
+        <div class="back_to_top">
+
+          <div class="arrow_btn">
+            <a class="btn" href="">
+              Back to top
+            </a>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <path
+                d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM13.53 10.03C13.38 10.18 13.19 10.25 13 10.25C12.81 10.25 12.62 10.18 12.47 10.03L10.75 8.31V13.5C10.75 13.91 10.41 14.25 10 14.25C9.59 14.25 9.25 13.91 9.25 13.5V8.31L7.53 10.03C7.24 10.32 6.76 10.32 6.47 10.03C6.18 9.74 6.18 9.26 6.47 8.97L9.47 5.97C9.76 5.68 10.24 5.68 10.53 5.97L13.53 8.97C13.82 9.26 13.82 9.74 13.53 10.03Z"
+                fill="#7175D8" />
+            </svg>
+
+          </div>
+        </div>
+        <div class="footer-logo-top">
+          <div class="footer-logo-area">
+
+            <div class="logo-container">
+              <a href="">
+                Bookinpro
+                <!-- <img class="logo" src="@/assets/images/logo.png" alt="logo"> -->
+              </a>
+            </div>
+            <p class="branding-text ">
+              imply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+            </p>
+            <div class="location">
+              <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                <path
+                  d="M14.0033 16.532C11.5183 16.532 9.48828 14.5137 9.48828 12.017C9.48828 9.52034 11.5183 7.51367 14.0033 7.51367C16.4883 7.51367 18.5183 9.53201 18.5183 12.0287C18.5183 14.5253 16.4883 16.532 14.0033 16.532ZM14.0033 9.26367C12.4866 9.26367 11.2383 10.5003 11.2383 12.0287C11.2383 13.557 12.4749 14.7937 14.0033 14.7937C15.5316 14.7937 16.7683 13.557 16.7683 12.0287C16.7683 10.5003 15.5199 9.26367 14.0033 9.26367Z"
+                  fill="#7175D8" />
+                <path
+                  d="M14.0019 26.5535C12.2752 26.5535 10.5369 25.9002 9.18358 24.6052C5.74191 21.2918 1.93858 16.0068 3.37358 9.7185C4.66858 4.0135 9.65025 1.4585 14.0019 1.4585C14.0019 1.4585 14.0019 1.4585 14.0136 1.4585C18.3652 1.4585 23.3469 4.0135 24.6419 9.73016C26.0652 16.0185 22.2619 21.2918 18.8202 24.6052C17.4669 25.9002 15.7286 26.5535 14.0019 26.5535ZM14.0019 3.2085C10.6069 3.2085 6.24358 5.01683 5.08858 10.1035C3.82858 15.5985 7.28191 20.3352 10.4086 23.3335C12.4269 25.2818 15.5886 25.2818 17.6069 23.3335C20.7219 20.3352 24.1752 15.5985 22.9386 10.1035C21.7719 5.01683 17.3969 3.2085 14.0019 3.2085Z"
+                  fill="#7175D8" />
+              </svg>
+              <p>
+                Dhaka, Bangladesh
+              </p>
+
+            </div>
+
+            <div class="subscribe">
+              <h5>
+                Get notified any update & news
+              </h5>
+
+              <div class="subs_mail">
+                <input type="email" placeholder="Your email here.." />
+
+                <a class="btn" href="#">
+                  Get qoute
+
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" viewBox="0 0 16 12" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                      d="M9.21967 0.21967C9.51256 -0.0732233 9.98744 -0.0732233 10.2803 0.21967L15.3387 5.278C15.6316 5.5709 15.6316 6.04577 15.3387 6.33866L10.2803 11.397C9.98744 11.6899 9.51256 11.6899 9.21967 11.397C8.92678 11.1041 8.92678 10.6292 9.21967 10.3363L13.7477 5.80833L9.21967 1.28033C8.92678 0.987437 8.92678 0.512563 9.21967 0.21967Z"
+                      fill="white" />
+                    <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd"
+                      d="M0 5.75C0 5.33579 0.335786 5 0.75 5H14.775C15.1892 5 15.525 5.33579 15.525 5.75C15.525 6.16421 15.1892 6.5 14.775 6.5H0.75C0.335786 6.5 0 6.16421 0 5.75Z"
+                      fill="white" />
+                  </svg>
+                </a>
+              </div>
+
+
+            </div>
+
+
+
+          </div>
+
+          <div class="footer-menu">
+
+            <div class="menus">
+              <div class="menu-column">
+                <div class="menu-title">
+                  General
+                </div>
+
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="/">Home</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Products</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Features</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Pricing</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Login</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Sign up</a>
+                </div>
+              </div>
+
+              <div class="menu-column">
+                <div class="menu-title">
+                  Services
+                </div>
+
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Create meeting</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Integration</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Embade</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Availability</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Notification</a>
+                </div>
+              </div>
+
+              <div class="menu-column">
+                <div class="menu-title">
+                  Company
+                </div>
+
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">About</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Blog</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Contact sales</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Privacy & policies</a>
+                </div>
+                <div class="menu-item">
+                  <a rel="noopener noreferrer" class="text-dark" href="">Terms & conditions</a>
+                </div>
+              </div>
+            </div>
+
+
+
+            <div class="social_links">
+              <a href="">
+                <img class="social_icon" src="/images/tour_booking/fb.svg">
+              </a>
+
+              <a href="">
+                <img class="social_icon" src="/images/tour_booking/fb.svg">
+              </a>
+
+              <a href="">
+                <img class="social_icon" src="/images/tour_booking/fb.svg">
+              </a>
+
+              <a href="">
+                <img class="social_icon" src="/images/tour_booking/fb.svg">
+              </a>
+
+            </div>
+          </div>
+
+        </div>
+        <div class="footer-bottom">
+          <p>©Bookinpro 2022. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="footer-top-main">
 
       <img class="footer-top-step"
         src="/images/tour_booking/footer-top-shape-mobile.svg">
@@ -11,8 +222,7 @@
 
           <div class="footer-top-left">
             <h2>Start for Free, Grow for Life</h2>
-            <p>At SerSkillInter, join a dynamic learning experience and gain the skills you need to thrive in any
-              career. Flexible, engaging, and designed to help you succeed</p>
+            <p>At Bookinpro, Unlock new possibilities with eye-catching websites and grow your dream. Lorem Ipsum is simply dummy text lorem Ipsum is simply dummy text </p>
           </div>
 
 
@@ -33,7 +243,7 @@
       </div>
     </div>
 
-    <div class="footer-main slide-up animate">
+    <div class="footer-main">
 
       <div class="container footer-logo-container">
         <div class="footer-logo-top">
@@ -90,7 +300,7 @@
           <p>Copyright © Bookinpro 2025 | All rights reserved.</p>
         </div>
       </div>
-    </div>
+    </div> -->
 
   </footer>
 </template>
